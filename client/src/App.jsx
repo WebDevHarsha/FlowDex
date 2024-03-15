a6af73f5f74615a40d25fd25bf9f8d01e98111c7
WebDevHarsha <vasriharsha@gmail.com>
2024-03-15 22:23:26 +0530

    added uploading and viewing pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,20 @@
 import React from "react"
 import Landing from "./pages/Landing"
 import SignUp from "./pages/SignUp"
+import Profile from "./pages/Profile"
+import Uploading from "./pages/Uploading"
+import Viewing from "./pages/Viewing"
+
 
 function App() {
 
   return (
     <div>
       {/* <Landing></Landing> */}
-      <SignUp></SignUp>
+      {/* <SignUp></SignUp> */}
+      {/* <Uploading></Uploading> */}
+      <Viewing></Viewing>
+      {/* <Profile></Profile> */}
     </div>
   )
 }
---
+++ client/src/pages/Uploading.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+function Uploading() {
+  return (
+    <div>
+        <Navbar />
+    <div className='h-screen w-screen flex bg-gradient-to-r from-[#fd0338] to-[#fbd895] '>
+      
+      <div className='flex justify-center w-1/2 items-center p-8 rounded-md'>
+            <h1 className=' text-3xl text-white font-[Roboto] '>
+            Transfer your files safely and get the review process done soon.
+            </h1>
+        </div>
+      <div className="flex justify-center w-1/2 items-center p-8 rounded-md">
+        
+        <form className="w-1/2 border-dashed border-black border-2 p-10" action="/upload" method="post" encType="multipart/form-data">
+          <label className="block text-lg font-bold text-gray-700 mb-4" htmlFor="document">Select a file to upload:</label>
+          <input
+            className="w-full border border-gray-300 rounded-md p-2 mb-4"
+            type="file"
+            id="document"
+            name="document"
+          />
+          <input className="w-full bg-green-500 text-white font-bold py-2 px-4 rounded-md cursor-pointer" type="submit" value="Upload" />
+        </form>
+      </div>
+    </div>
+    </div>
+  );
+}
+
+export default Uploading;
