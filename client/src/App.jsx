8002e8ef8a196039e4a0ce60290cc487a65ed17e
WebDevHarsha <vasriharsha@gmail.com>
2024-03-16 00:24:37 +0530

    added connect wallet functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,24 @@
 import React from "react"
 import Landing from "./pages/Landing"
-import SignUp from "./pages/SignUp"
+import SignUp from "./pages/SignUp" 
 import Profile from "./pages/Profile"
 import Uploading from "./pages/Uploading"
 import Viewing from "./pages/Viewing"
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
 
   return (
-    <div>
-      {/* <Landing></Landing> */}
-      {/* <SignUp></SignUp> */}
-      {/* <Uploading></Uploading> */}
-      <Viewing></Viewing>
-      {/* <Profile></Profile> */}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing/>} />
+        <Route path="/signup" element={<SignUp/>}/> 
+        <Route path="/upload" element={<Uploading />}/>
+        <Route path="/viewing" element={<Viewing/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+
+      </Routes>    
+    </BrowserRouter>
   )
 }
 
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React,{useState}  from 'react'
+import Navbar from '../components/Navbar'
+import { ethers } from "ethers";
+
 
 function Profile() {
+  const [data, setdata] = useState({
+    address: "",
+    Balance: null,
+});
+const btnhandler = () => {
+  if (window.ethereum) {
+      window.ethereum
+          .request({ method: "eth_requestAccounts" })
+          .then((res) => accountChangeHandler(res[0]))
+          .catch((error) => console.error(error)); // Add error handling here
+  } else {
+      alert("install metamask extension!!");
+  }
+};
+
+const getbalance = (address) => {
+  window.ethereum
+      .request({
+          method: "eth_getBalance",
+          params: [address, "latest"],
+      })
+      .then((balance) => {
+          setdata({
+              Balance: ethers.utils.formatEther(balance),
+          });
+      })
+      .catch((error) => console.error(error)); // Add error handling here
+};
+
+const accountChangeHandler = (account) => {
+  setdata({
+      address: account,
+  });
+
+  getbalance(account);
+};
   return (
     <div>
-      
+      <Navbar></Navbar>
+      <div className='mt-20'>
+        <h1 className='text-red-500'>
+        {data.address}
+        </h1>
+        <h1 className='text-red-600'>
+        {data.balance}
+        </h1>
+        <button
+                        onClick={btnhandler}
+                        variant="primary"
+                    >
+                        Connect to wallet
+                    </button>
+      </div>
     </div>
   )
 }
